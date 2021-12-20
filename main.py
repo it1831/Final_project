c911dea75b55efe5a821cbc190327c9057bf7f5d
it1831 <47101311+it1831@users.noreply.github.com>
2021-12-20 21:29:49 +0100

    Update main.py
    add time to output

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import datetime
 import cv2
 from matplotlib import pyplot as plt
 import numpy as np
@@ -45,8 +45,9 @@ text = result[0][-2]
 font = cv2.FONT_HERSHEY_SIMPLEX
 res = cv2.putText(img, text=text, org=(approx[0][0][0], approx[1][0][1]+60), fontFace=font, fontScale=1, color=(0,255,0), thickness=2, lineType=cv2.LINE_AA)
 f = open("demofile.txt", "w")
-time = datetime.datetime.now()
-f.write(text)
+time = datetime.now()
+#print(time)
+f.write(text + "    " + str(time))
 f.close()
 res = cv2.rectangle(img, tuple(approx[0][0]), tuple(approx[2][0]), (0,255,0),3)
 plt.imsave('image_new.jpg', cv2.cvtColor(res, cv2.COLOR_BGR2RGB))
