125dc87e0ac45de8cb4c66cd7a5d927b95970f4c
it1831 <47101311+it1831@users.noreply.github.com>
2022-01-08 14:25:12 +0100

    slight changes to look of code

--- main.py
+++ main.py
@@ -13,8 +13,7 @@ video = cv2.VideoCapture('/home/jouadamis/PycharmProjects/pythonProject/video1.m
 
 
 #read frame by frame
-
-success,image = video.read()
+success, image = video.read()
 #define variables
 count = 0
 frames = 1
@@ -27,25 +26,21 @@ while success:
         cv2.imwrite("frame%d.jpg" % count, image)
         #my trie to save image as variable not a .jpg
         #img = plt.Image.open(image)
-        success,image = video.read()
+        success, image = video.read()
         #add plus one to variables to use another frame and save next image with different name
         count += 1
         frames += 1
-
     else:
         frames += 1
-
-
-
 
 #save image to variable
 #image = pil.Image.open('image')
 #loop whole code to read every saved frame
 number = 0
-
+#loop for reading only frames made from video
 for number in range(count):
     #define variable img for used image
-    img = cv2.imread('/home/jouadamis/PycharmProjects/pythonProject/frame%d.jpg' %number)
+    img = cv2.imread('/home/jouadamis/PycharmProjects/pythonProject/frame%d.jpg' % number)
     #img = cv2.imread('/home/jouadamis/PycharmProjects/pythonProject/image1.jpg')
 
     #converting picture to gray
@@ -53,9 +48,9 @@ for number in range(count):
     #Apply filter and find edges for localization
 
     #using bilateral Filter that reduces noise
-    bfilter = cv2.bilateralFilter(gray, 11, 17, 17) #Noise reduction
+    bfilter = cv2.bilateralFilter(gray, 11, 17, 17)
     #detecting edges
-    edged = cv2.Canny(bfilter, 30, 200) #Edge detection
+    edged = cv2.Canny(bfilter, 30, 200)
 
     #find edges
     keypoints = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
@@ -74,12 +69,12 @@ for number in range(count):
 
     mask = np.zeros(gray.shape, np.uint8)
     #finding numberplate
-    new_image = cv2.drawContours(mask, [location], 0,255, -1)
+    new_image = cv2.drawContours(mask, [location], 0, 255, -1)
     #overlaying everything except numberplate
     new_image = cv2.bitwise_and(img, img, mask=mask)
 
     #storing coordinates in variables x,y
-    (x,y) = np.where(mask==255)
+    (x, y) = np.where(mask == 255)
     #storing top left point(min) and bottom right point(max)
     (x1, y1) = (np.min(x), np.min(y))
     (x2, y2) = (np.max(x), np.max(y))
@@ -93,7 +88,7 @@ for number in range(count):
     result = reader.readtext(cropped_image)
 
     f = open("file.txt", "a")
-    f.write(str(result)  + " \n")
+    f.write(str(result) + " \n")
     f.close()
 
     #Render Result
@@ -104,19 +99,21 @@ for number in range(count):
     #define font on picture
     font = cv2.FONT_HERSHEY_SIMPLEX
     #putting text in picture
-    res = cv2.putText(img, text=text , org=(approx[0][0][0], approx[1][0][1]+60), fontFace=font, fontScale=1, color=(0,255,0), thickness=2, lineType=cv2.LINE_AA)
+    res = cv2.putText(img, text=text, org=(approx[0][0][0], approx[1][0][1]+60), fontFace=font, fontScale=1,
+                      color=(0, 255, 0), thickness=2, lineType=cv2.LINE_AA)
     #open txt file
     f = open("demofile.txt", "a")
     #define date + time to variable time
     time = datetime.now()
     #write in file SPZ and date+time
-    f.write(text +"    " + str(time) + " \n")
+    f.write(text + "    " + str(time) + " \n")
     #close file
     f.close()
     #drawing rectangle in picture
-    res = cv2.rectangle(img, tuple(approx[0][0]), tuple(approx[2][0]), (0,255,0),3)
+    res = cv2.rectangle(img, tuple(approx[0][0]), tuple(approx[2][0]), (0, 255, 0), 3)
     #save image
-    plt.imsave('/home/jouadamis/PycharmProjects/pythonProject/image_new%d.jpg' %number ,  cv2.cvtColor(res, cv2.COLOR_BGR2RGB))
+    plt.imsave('/home/jouadamis/PycharmProjects/pythonProject/image_new%d.jpg' % number,
+               cv2.cvtColor(res, cv2.COLOR_BGR2RGB))
     print(number)
     #save image
     #plt.imsave('image_new1.jpg', cv2.cvtColor(res, cv2.COLOR_BGR2RGB))
